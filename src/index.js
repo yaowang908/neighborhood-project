8511e0cfa60cac6acd3c3484ab79cbe2010d11b1
Yao Wang <yao422321065@yahoo.com>
2018-11-04 15:25:50 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let ViewModel = function(first,last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last);
+
+    this.fullName = ko.computed(function(){
+        return this.firstName()+" "+this.lastName();
+    },this);
+};
+ko.applyBindings(new ViewModel("Planet","Earth"));
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const webpack = require("webpack");
+const distPath = path.resolve(__dirname,"dist/");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry:{
+        app:"./src/index.js",
+        jquery:"jquery",
+        knockout:"knockout"
+    },
+    devtool:'source-map',
+    output:{
+        path:distPath,
+        filename:"[name].bundle.js"
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/(node_modules|bower_components)/,
+                use:{
+                    loader:'babel-loader',
+                    options:{presets:['env']}
+                }
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
+                ]
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                use:[
+                    "file-loader"
+                ]
+            },
+            {
+                test:/\.(css|scss|sass)$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve:{
+        extensions:['*','.js','.jsx']
+    },
+    node:{
+        __dirname:false,
+        __filename:false
+    },
+    devServer:{
+        stats:'errors-only',
+        contentBase:'./dist',
+        port:3000
+    },
+    optimization:{
+        nodeEnv: 'production',
+
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist/*.*']),
+        new webpack.ProvidePlugin({
+            $:"jquery",
+            jQuery:'jquery',
+            ko:'knockout'
+        }),
+        new webpack.HotModuleReplacementPlugin([
+            path.join(__dirname,'node_modules')
+        ]),
+        new HtmlWebpackPlugin({
+            title:'Neighborhood Map',
+            template: path.resolve(__dirname,'src/index.html')
+        }),
+        new CompressionPlugin({
+            test:/\.js(\?.*)?$/i
+        })
+    ]
+}
