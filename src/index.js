05efc36f1bcf49909ab44fde17139581e894f2d6
Yao Wang <yao422321065@yahoo.com>
2018-11-04 22:07:59 -0500

    Test here.com api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-let ViewModel = function(first,last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
+import './styles/main.scss';
+import $ from 'jquery';
 
-    this.fullName = ko.computed(function(){
-        return this.firstName()+" "+this.lastName();
-    },this);
-};
-ko.applyBindings(new ViewModel("Planet","Earth"));
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 40.750, lng: -74.001 },//Manhattan Midtown NY
+        zoom: 13
+    });
+}
+
+window.initMap = initMap;
+
+//inject google map api  
+$("body").append('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDeFIT4z8Ef8rVvIUq2NfiUX3LOko5yNso&callback=initMap" async defer ></script >');
