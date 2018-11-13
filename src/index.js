2081fe1f6849e4b6f8c05ed58570f8c4ece7b866
Yao Wang <yao422321065@yahoo.com>
2018-11-13 00:30:19 -0500

    redesigned markersviewmodel with js class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/main.scss';
 import hereApiMapEvents from './Components/mapevent/index';
 import mapInit from './Components/mapinit/index';
+import defaultLocations from './Components/mapinit/defaultMarkers.js';
 import loadding from './Components/loading/index';
 import sidePanel from './Components/sidePanel/index';
 import $ from 'jquery';
@@ -19,67 +20,95 @@ function MarkerModel(location) {
     this.closeButtonEnabled = ko.observable(false);
 }
 
-function MarkersViewModel(platform, defaultLayers, map, ui, myMapEvent) {
-    let self = this;
+class MarkersViewModel {
+    constructor(platform, defaultLayers, map, ui, myMapEvent,defaultLocations){
+        let self = this;
+        self.platform = platform;
+        self.defaultLayers = defaultLayers;
+        self.map = map;
+        self.ui = ui;
+        self.myMapEvent = myMapEvent;
+        self.locations = defaultLocations;
+        let tempMarkers = $.map(self.locations, function (item) { return new MarkerModel(item) });
+        self.markers = ko.observableArray(tempMarkers);
 
-    self.locations = [
-        { name: 'Duke Eatery', address:'300 Park Ave, New York, NY 10022',type:'restaurant',iconLetter:'F'},
-        { name: 'Serendipity 3', address: '225 E 60th St, New York, NY 10022', type: 'restaurant', iconLetter: 'F'},
-        { name: 'Soju Haus', address: '315 5th Ave Fl 2, New York, NY 10016', type: 'restaurant', iconLetter: 'F'},
-        { name: 'China Town', address: 'China town, Manhattan, NY', type: 'district', iconLetter: 'D'},
-        { name: 'Midtown', address: 'Midtown, Manhattan, NY', type: 'district', iconLetter: 'D'},
-        { name: 'Chelsea', address: 'Chelsea, Manhattan, NY', type: 'district', iconLetter: 'D'}
-    ];
+        self.onMapObjects = [];
+        self.init().then(function (result) {//call init, promise ends, returned all markers
+            console.dir(result);
+            // console.log(result[0].icon.i.classList[1]);//get marker class name
+            self.onMapObjects = result;
+        }).catch((err) => { alert(err) });
+    }
 
-    let tempMarkers = $.map(self.locations,function(item){return new MarkerModel(item)});
-
-    self.markers = ko.observableArray(tempMarkers);//
-
-    self.createMarkerDomTemplate = function (markerClass, markerIconLetter) {
+    createMarkerDomTemplate(markerClass, markerIconLetter) {
         //marker icon template 
         return '<div class="markers ' + markerClass + '"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect stroke="white" fill="#1b468d" x="1" y="1" width="22" height="22" /><text x="12" y="18" font-size="12pt" font-family="Arial" font-weight="bold" text-anchor="middle" fill="white">' + markerIconLetter + '</text></svg></div>';
     }
-    
-    self.init = function (){
+
+    showDetail(marker, pureLatLng = false) {
+        //when clicked show corresponding infowindow with details about location
+        //TODO: 
+        //1.template of infowindow;
+        //1.5: create marker property to hold state of infobubble like isVisible
+        //2.API to 3rd party services for details 
+        if (!pureLatLng) {
+            //passed in address, 
+            //get location
+            this.myMapEvent.getMarkersLatLng(marker.address, platform).then((location) => {
+                let bubble = new H.ui.InfoBubble(location, {
+                    content: '<b>Hellow World</b>'
+                });
+                ui.addBubble(bubble);
+            }).catch((err) => { alert(err) })
+        } else {
+            //passed in latlng pair
+            let bubble = new H.ui.InfoBubble(marker, {
+                content: '<b>Hellow World</b>'
+            });
+            ui.addBubble(bubble);
+        }
+
+    } //end of showDetail
+
+    init() {
+        let self = this;
         sidePanel();//add sidepanel
         //markers init
-        return Promise.all( $.map(self.markers(),function(item,index){
-                //create each marker on map
-                let addressToSearch = item.address;
-                let markerDomTemplate = self.createMarkerDomTemplate(item.className + " " + item.type, item.iconLetter);
-                return myMapEvent.createMarker(map, addressToSearch, platform, markerDomTemplate);
-            })
-        ).then(()=>{
-            let objects = map.getObjects();
+        return Promise.all($.map(self.markers(), function (item, index) {
+            //create each marker on map
+            let addressToSearch = item.address;
+            let markerDomTemplate = self.createMarkerDomTemplate(item.className + " " + item.type, item.iconLetter);
+            return self.myMapEvent.createMarker(self.map, addressToSearch, self.platform, markerDomTemplate);
+        })
+        ).then(() => {
+            let objects = this.map.getObjects();
+            $.map(objects, function (item, index) {
+                item.addEventListener('click', function () { console.log('yes!Im clicked') });
+                //FIXME: addEventListener is not working
+                //******Click event must monitored by knockout */
+            });
             //get all object added to map
             return objects;
-        }).catch((err)=>{alert(err);});
+        }).catch((err) => { alert(err); });
     } // end of init
 
-    self.onMapObjects = [];
-    self.init(self.mapCenterText).then(function(result){//call init, promise ends, returned all markers
-        console.dir(result);
-        // console.log(result[0].icon.i.classList[1]);//get marker class name
-        self.onMapObjects = result;
-    }).catch((err)=>{alert(err)});
-
-    self.showCloseButton = function(marker) {
+    showCloseButton(marker) {
         //show hide marker button
         marker.closeButtonEnabled(true);
     }
-    self.hideCloseButton = function(marker) {
+    hideCloseButton(marker) {
         //hide 'hide marker button' when mouse leave marker entry on side panel
         marker.closeButtonEnabled(false);
     }
 
-    self.hideMarker = function(marker){
-        if(self.onMapObjects) {
+    hideMarker(marker) {
+        if (this.onMapObjects) {
             marker.isVisible(false);
             let markerObject;
-            for(let i=0;i<self.onMapObjects.length;i++) {
+            for (let i = 0; i < this.onMapObjects.length; i++) {
                 // console.log(self.onMapObjects[i].icon.i.classList[1]);
-                if (self.onMapObjects[i].icon.i.classList[1] === marker.className){
-                    markerObject = self.onMapObjects[i];
+                if (this.onMapObjects[i].icon.i.classList[1] === marker.className) {
+                    markerObject = this.onMapObjects[i];
                 }
             }
             markerObject.setVisibility(false);
@@ -90,22 +119,115 @@ function MarkersViewModel(platform, defaultLayers, map, ui, myMapEvent) {
             //if on objects are added to map
             alert('Be patient!');
         }
-    }
+    }//end of hideMarker
 
-    self.showDetail = function(marker) {
-        //when clicked show corresponding infowindow with details about location
-        //TODO: 
-        //1.template of infowindow;2.API to 3rd party services for details 
+}// end of viewModel class
 
-        //get location
-        myMapEvent.getMarkersLatLng(marker.address,platform).then((location)=>{
-            let bubble = new H.ui.InfoBubble(location,{
-                content:'<b>Hellow World</b>'
-            });
-            ui.addBubble(bubble);
-        }).catch((err)=>{alert(err)})
-    };
-}
+// function MarkersViewModel(platform, defaultLayers, map, ui, myMapEvent) {
+//     let self = this;
+
+//     self.locations = [
+//         { name: 'Duke Eatery', address:'300 Park Ave, New York, NY 10022',type:'restaurant',iconLetter:'F'},
+//         { name: 'Serendipity 3', address: '225 E 60th St, New York, NY 10022', type: 'restaurant', iconLetter: 'F'},
+//         { name: 'Soju Haus', address: '315 5th Ave Fl 2, New York, NY 10016', type: 'restaurant', iconLetter: 'F'},
+//         { name: 'China Town', address: 'China town, Manhattan, NY', type: 'district', iconLetter: 'D'},
+//         { name: 'Midtown', address: 'Midtown, Manhattan, NY', type: 'district', iconLetter: 'D'},
+//         { name: 'Chelsea', address: 'Chelsea, Manhattan, NY', type: 'district', iconLetter: 'D'}
+//     ];
+
+//     let tempMarkers = $.map(self.locations,function(item){return new MarkerModel(item)});
+
+//     self.markers = ko.observableArray(tempMarkers);//
+
+//     self.createMarkerDomTemplate = function (markerClass, markerIconLetter) {
+//         //marker icon template 
+//         return '<div class="markers ' + markerClass + '"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><rect stroke="white" fill="#1b468d" x="1" y="1" width="22" height="22" /><text x="12" y="18" font-size="12pt" font-family="Arial" font-weight="bold" text-anchor="middle" fill="white">' + markerIconLetter + '</text></svg></div>';
+//     }
+
+//     self.showDetail = function (marker,pureLatLng = false) {
+//         //when clicked show corresponding infowindow with details about location
+//         //TODO: 
+//         //1.template of infowindow;
+//         //1.5: create marker property to hold state of infobubble like isVisible
+//         //2.API to 3rd party services for details 
+//         if(!pureLatLng){
+//             //passed in address, 
+//             //get location
+//             myMapEvent.getMarkersLatLng(marker.address, platform).then((location) => {
+//                 let bubble = new H.ui.InfoBubble(location, {
+//                     content: '<b>Hellow World</b>'
+//                 });
+//                 ui.addBubble(bubble);
+//             }).catch((err) => { alert(err) })
+//         } else {
+//             //passed in latlng pair
+//             let bubble = new H.ui.InfoBubble(marker, {
+//                 content: '<b>Hellow World</b>'
+//             });
+//             ui.addBubble(bubble); 
+//         }
+        
+//     };
+    
+//     self.init = function (){
+//         sidePanel();//add sidepanel
+//         //markers init
+//         return Promise.all( $.map(self.markers(),function(item,index){
+//                 //create each marker on map
+//                 let addressToSearch = item.address;
+//                 let markerDomTemplate = self.createMarkerDomTemplate(item.className + " " + item.type, item.iconLetter);
+//                 return myMapEvent.createMarker(map, addressToSearch, platform, markerDomTemplate);
+//             })
+//         ).then(()=>{
+//             let objects = map.getObjects();
+//             $.map(objects,function(item,index){
+//                 item.addEventListener('click',function(){console.log('yes!Im clicked')});
+//                 //FIXME: addEventListener is not working
+//                 //******Click event must monitored by knockout */
+//             });
+//             //get all object added to map
+//             return objects;
+//         }).catch((err)=>{alert(err);});
+//     } // end of init
+
+//     self.onMapObjects = [];
+//     self.init(self.mapCenterText).then(function(result){//call init, promise ends, returned all markers
+//         console.dir(result);
+//         // console.log(result[0].icon.i.classList[1]);//get marker class name
+//         self.onMapObjects = result;
+//     }).catch((err)=>{alert(err)});
+
+//     self.showCloseButton = function(marker) {
+//         //show hide marker button
+//         marker.closeButtonEnabled(true);
+//     }
+//     self.hideCloseButton = function(marker) {
+//         //hide 'hide marker button' when mouse leave marker entry on side panel
+//         marker.closeButtonEnabled(false);
+//     }
+
+//     self.hideMarker = function(marker){
+//         if(self.onMapObjects) {
+//             marker.isVisible(false);
+//             let markerObject;
+//             for(let i=0;i<self.onMapObjects.length;i++) {
+//                 // console.log(self.onMapObjects[i].icon.i.classList[1]);
+//                 if (self.onMapObjects[i].icon.i.classList[1] === marker.className){
+//                     markerObject = self.onMapObjects[i];
+//                 }
+//             }
+//             markerObject.setVisibility(false);
+//             // map.removeObject(markerObject);
+//             // console.log(marker.name+' '+marker.isVisible());
+//             // console.log(marker.className);
+//         } else {
+//             //if on objects are added to map
+//             alert('Be patient!');
+//         }
+//     }
+
+   
+// }
 //KO only need data-bind , it's possible been done by changing template in createMaker.js
 
 $(document).ready(function(){
@@ -119,7 +241,7 @@ $(document).ready(function(){
         let myMapEvent = new hereApiMapEvents(map);
         myMapEvent.whenYouTap();
 
-        let markerViewModel = new MarkersViewModel(platform, defaultLayers, map, ui, myMapEvent);
+        let markerViewModel = new MarkersViewModel(platform, defaultLayers, map, ui, myMapEvent, defaultLocations);
         ko.applyBindings(markerViewModel);
     });
 });
